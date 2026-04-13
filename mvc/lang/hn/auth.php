9455347da5e200d794821bf7a208c441b0af89d8
Shaan Saurav <shaansaurav633@gmail.com>
2026-04-13 13:41:25 +0530

    Add language files for authentication, pagination, passwords, and validation in Hindi

---
+++ mvc/lang/hn/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ये साख जानकारी हमारे रिकॉर्ड से मेल नहीं खाती।',
+    'password' => 'दिया गया पासवर्ड गलत है।',
+    'throttle' => 'बहुत सारे लॉगिन प्रयास। कृपया :seconds सेकंड में फिर से कोशिश करें।',
+
+];
